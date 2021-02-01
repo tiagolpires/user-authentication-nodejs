72ad9567d09b50849f86b011cf92ecc55491bf2b
tiagolpires <tiagolpires@hotmail.com>
2021-02-01 14:25:09 -0300

    Initial

--- src/routes.js
+++ src/routes.js
@@ -10,8 +10,4 @@ routes.get('/register', userController.registerPage)
 routes.post('/', userController.login)
 routes.post('/register', userController.register)
 
-routes.get('/loginSucess', (req, res) => {
-    res.render('loginSucess.ejs', {name: 'Tiagoo', email: 'taigolsadasd@hotmail.com', date: '21/02/2002'})
-})
-
 module.exports = routes
